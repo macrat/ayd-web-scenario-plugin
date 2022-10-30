c3c5fdc61ff362afc20d691644203105df903ec5
MacRat <m@crat.jp>
2022-10-30 20:36:18 +0900

    perf: add flag to enable cpuprofile

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/pprof"
 	"time"
 
 	"github.com/macrat/ayd-web-scenario-plugin/internal"
@@ -38,6 +39,9 @@ func main() {
 	showVersion := flags.BoolP("version", "v", false, "show version and exit.")
 	showHelp := flags.BoolP("help", "h", false, "show help message and exit.")
 
+	cpuprofile := flags.String("cpuprofile", "", "path to cpu profile.")
+	flags.MarkHidden("cpuprofile")
+
 	if err := flags.Parse(os.Args[1:]); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		fmt.Fprintf(os.Stderr, "\nPlease see `%s -h` for more information.\n", os.Args[0])
@@ -51,6 +55,18 @@ func main() {
 		fmt.Println("$ ayd-web-scenario-plugin [OPTIONS] TARGET_URL\n\nOptions:")
 		flags.PrintDefaults()
 		return
+	}
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "failed to create profile file.")
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
 	}
 
 	target, err := ParseTargetURL(flags.Arg(0))
