f23279b39af5da956c7899e4546363b33de0d734
MacRat <m@crat.jp>
2022-10-31 21:19:44 +0900

    test: fix file extension in test script

--- internal/testdata/screenshot.lua
+++ internal/testdata/screenshot.lua
@@ -3,9 +3,9 @@ t = tab.new(TEST.url())
 assert.eq(io.popen("ls " .. TEST.storage()):read("*a"), "")
 
 t:screenshot()
-assert.eq(io.popen("ls " .. TEST.storage()):read("*a"), "000001.jpg\n")
+assert.eq(io.popen("ls " .. TEST.storage()):read("*a"), "000001.png\n")
 
 t("b"):screenshot()
-assert.eq(io.popen("ls " .. TEST.storage()):read("*a"), "000001.jpg\n000002.jpg\n")
+assert.eq(io.popen("ls " .. TEST.storage()):read("*a"), "000001.png\n000002.png\n")
 
 -- XXX: This test works on only UNIX. It doesn't work on Windows.
